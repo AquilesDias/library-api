f961fafadd9abb0613659e7793d1f8baf8f27ea5
AquilesDias <kilesax@gmail.com>
2023-05-23 18:50:24 -0300

    Agendamento de tarefas com Schedules do Spring.

--- src/main/java/com/github/aquilesdias/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/github/aquilesdias/libraryapi/LibraryApiApplication.java
@@ -4,14 +4,21 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.ui.ModelMap;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class LibraryApiApplication {
 
 	@Bean
 	public ModelMapper modelmapper(){
 		return new ModelMapper();
+	}
+
+	@Scheduled(cron = "0 14 18 1/1 * ?")
+	public void testeDoScheldues(){
+		System.out.println("ALERTA: HORARIO DE AGENDAMENTO");
 	}
 
 	public static void main(String[] args) {
