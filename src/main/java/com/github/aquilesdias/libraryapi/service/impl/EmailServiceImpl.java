17453f2542f6d7e662bb2e545ef09a5cb977e723
AquilesDias <kilesax@gmail.com>
2023-05-26 08:53:10 -0300

    Implementando o serviço de email

--- src/main/java/com/github/aquilesdias/libraryapi/service/impl/EmailServiceImpl.java
+++ src/main/java/com/github/aquilesdias/libraryapi/service/impl/EmailServiceImpl.java
@@ -1,15 +1,34 @@
 package com.github.aquilesdias.libraryapi.service.impl;
 
 import com.github.aquilesdias.libraryapi.service.EmailService;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    @Value("${ application.mail.default-remetent }")
+    private String remetent;
 
     @Override
     public void sendMails(String messageLateLoan, List<String> listEmails) {
 
+        String [] emails = listEmails.toArray(new String[ listEmails.size()]);
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+        simpleMailMessage.setFrom(remetent);
+        simpleMailMessage.setSubject("Livro atrasado.");
+        simpleMailMessage.setText(messageLateLoan);
+        simpleMailMessage.setTo(emails);
     }
 }
