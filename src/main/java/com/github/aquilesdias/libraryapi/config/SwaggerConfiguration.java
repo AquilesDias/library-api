b1ef48996ce00f9c5996155c779b62ec99486711
AquilesDias <kilesax@gmail.com>
2023-05-29 10:03:13 -0300

    Documentando API com Swagger.

---
+++ src/main/java/com/github/aquilesdias/libraryapi/config/SwaggerConfiguration.java
@@ -0,0 +1,44 @@
+package com.github.aquilesdias.libraryapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfiguration {
+
+
+    @Bean
+    public Docket docket (){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.github.aquilesdias.libraryapi.api.resource"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo () {
+        return new ApiInfoBuilder()
+                .title("Library API")
+                .description("API do projeto de controle de aluguel de livros.")
+                .version("1.0.0")
+                .contact(contact())
+                .build();
+    }
+    
+    private Contact contact () {
+        return new Contact(
+                "Aquiles Dias",
+                "https://github.com/AquilesDias/library-api",
+                "kilesax@gmail.com" );
+    }
+}
